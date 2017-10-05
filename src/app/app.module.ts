55f5b989b5b5259ff4838d86ead34312712f30f9
Manoj <mano42023@gmail.com>
2017-10-05 19:11:20 +0530

    newqr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 
 @NgModule({
   declarations: [
@@ -24,6 +24,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    BarcodeScanner,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,46 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BarcodeScanner ,BarcodeScannerOptions } from '@ionic-native/barcode-scanner';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+scanData : {};
+encodeData : string ;
+encodedData : {} ;
+options :BarcodeScannerOptions;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,private barcodeScanner: BarcodeScanner) {
 
   }
+
+scan(){
+    this.options = {
+        prompt : "Scan your barcode "
+    }
+    this.barcodeScanner.scan(this.options).then((barcodeData) => {
+
+        console.log(barcodeData);
+        this.scanData = barcodeData;
+    }, (err) => {
+        console.log("Error occured : " + err);
+    });         
+}    
+
+
+encodeText(){
+    this.barcodeScanner.encode(this.barcodeScanner.Encode.TEXT_TYPE,this.encodeData).then((encodedData) => {
+
+        console.log(encodedData);
+        this.encodedData = encodedData;
+
+    }, (err) => {
+        console.log("Error occured : " + err);
+    });     
+}
+
+
 
 }
