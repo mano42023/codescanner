9161308e2a38e6d644ad90e5f374aea51252d9e6
Manoj <mano42023@gmail.com>
2017-10-05 19:24:06 +0530

    newqr

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,46 +1,25 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { BarcodeScanner ,BarcodeScannerOptions } from '@ionic-native/barcode-scanner';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+
+ selector: 'page-home',
+  providers: [BarcodeScanner],
+ templateUrl: 'home.html'
 })
+
 export class HomePage {
-scanData : {};
-encodeData : string ;
-encodedData : {} ;
-options :BarcodeScannerOptions;
+	public barcode:string;
+ constructor(public navCtrl: NavController , private barcodeScanner: BarcodeScanner ) {
+   }
+   
 
-  constructor(public navCtrl: NavController,private barcodeScanner: BarcodeScanner) {
-
-  }
-
-scan(){
-    this.options = {
-        prompt : "Scan your barcode "
-    }
-    this.barcodeScanner.scan(this.options).then((barcodeData) => {
-
-        console.log(barcodeData);
-        this.scanData = barcodeData;
-    }, (err) => {
-        console.log("Error occured : " + err);
-    });         
-}    
-
-
-encodeText(){
-    this.barcodeScanner.encode(this.barcodeScanner.Encode.TEXT_TYPE,this.encodeData).then((encodedData) => {
-
-        console.log(encodedData);
-        this.encodedData = encodedData;
-
-    }, (err) => {
-        console.log("Error occured : " + err);
-    });     
-}
-
-
-
+ scan(){
+	this.barcodeScanner.scan().then((barcodeData) => {
+		this.barcode= barcodeData.text;  
+	}, (err) => {
+		this.barcode=err;
+	});
+ }
 }
